861fb94eff47f140d70dff4f66dc01ae6274954c
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-02-20 16:21:29 +0900

    2022.02.20 boardWrite메서드 변경 변경내용 :  - idx에 따라 다르게 동작하도록 로직 구현
    registerBoard 메서드 추가
    추가내용 :
     - 게시글 신규등록/수정 하도록 메서드 추가
     - 기타 에러처리는 구현 예정

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.BoardDTO;
 import com.example.demo.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
@@ -13,7 +17,31 @@ public class BoardController {
     BoardService boardService;
 
     @GetMapping(value = "/board/write.do")
-    public String boardWrite(Model model){
+    public String boardWrite(Model model, @RequestParam(value = "idx", required = false) Long idx){
+        if(idx == null){
+            model.addAttribute("board", new BoardDTO());
+        }else{
+            BoardDTO board = boardService.getBoardDetail(idx);
+            if(board == null){
+                return "redirect:/board/list.do";
+            }
+            model.addAttribute("board", board);
+        }
         return "board/write";
+    }
+
+    @PostMapping(value = "/board/register.do")
+    public String registerBoard(final BoardDTO board){
+        try{
+            boolean isRegister = boardService.registerBoard(board);
+            if(isRegister == false){
+                // 게시글 등록에 실패
+            }
+        }catch (DataAccessException e){
+            // DB처리과정에 문제 발생
+        }catch (Exception e){
+            // 시스템 문제 발생
+        }
+        return "redirect:/board/list.do";
     }
 }
